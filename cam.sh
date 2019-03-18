f9d5aba7df2c8f13149f423def36e031a06673ba
aptyp222 <aptyp222@me.com>
2019-03-18 17:49:09 +0400

    Create cam.sh

---
+++ cam.sh
@@ -0,0 +1,20 @@
+sudo apt-get install git -y
+sudo apt-get install pkg-config autoconf automake libtool libx264-dev
+
+git clone https://github.com/mstorsjo/fdk-aac.git
+cd fdk-aac
+./autogen.sh
+./configure --prefix=/usr/local --enable-shared --enable-static
+make -j4
+sudo make install
+sudo ldconfig
+cd ..
+
+git clone https://github.com/FFmpeg/FFmpeg.git
+cd FFmpeg
+./configure --prefix=/usr/local --arch=armel --target-os=linux --enable-omx-rpi --enable-nonfree --enable-gpl --enable-libfdk-aac --enable-mmal --enable-libx264 --enable-decoder=h264 --enable-network --enable-protocol=tcp --enable-demuxer=rtsp
+make -j4
+sudo make install
+cd ..
+
+sudo npm install -g homebridge-camera-ffmpeg
